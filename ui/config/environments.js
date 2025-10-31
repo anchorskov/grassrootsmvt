ca44591f4194439ab19e95890aeabd346b508dcf
Jim Skovgard <anchorskov@rmail.com>
2025-10-31 05:17:25 -0600

    working in producion not local

--- ui/config/environments.js
+++ ui/config/environments.js
@@ -1,5 +1,8 @@
 (() => {
   window.environmentConfig = {
+    shouldBypassAuth() {
+      return /^(localhost|127\.0\.0\.1)$/.test(location.hostname);
+    },
     getApiUrl(endpoint = "", params = {}) {
       const clean = String(endpoint).replace(/^\/?api\/?/, "");
       const qs = new URLSearchParams(params);
